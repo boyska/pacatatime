9646d275f2a5176ced944393892541c28ed2a55f
Davide Lo Re <boyska@gmail.com>
2009-06-11 22:09:16 +0200

    basic support for graphs, leafs, sequence...
    atm it is able (given a set of packages) to build its dependency graph,
    find leaves, setting "properties" and extracting a valid install sequence

--- pacatatime.py
+++ pacatatime.py
@@ -8,10 +8,188 @@ import popen2
 from optparse import OptionParser
 import urlparse
 import re
+from subprocess import Popen, PIPE
 
+from collections import defaultdict
+
+DB_PATH = '/var/lib/pacman'
 BASE_DIR = "/tmp/pacatatime/cache"
-PKG_URL = re.compile('.*/(.*)-[\d.]+-\d+-\w+\.pkg\..*')
+PKG_URL = re.compile(
+        '.*/(.*?)/os/.*/(.*)-([\d.]+-\d+)-\w+\.pkg\..*', re.UNICODE)
 
+class DiGraph(object):
+    def __init__(self):
+        self.nodes = set()
+        self.edges = defaultdict(set) #'name': {set, of, adiacents}
+        self.vertex_properties = defaultdict(set) #'name': {set, of, properties}
+        #(from, to): {set, of, properties}
+        self.edge_properties = defaultdict(set)
+
+    def add_vertex(self, name):
+        self.nodes.add(name)
+
+    def add_edge(self, node_from, node_to):
+        self.nodes.add(node_from)
+        self.nodes.add(node_to)
+        self.edges[node_from].add(node_to)
+
+    def get_adiacents(self, node):
+        return self.edges[node]
+
+    def pop_vertex(self, name):
+        self.nodes.remove(name)
+        del self.edges[name]
+
+    def remove_edge(self, node_from, node_to):
+        adiacents = self.get_adiacents(node_from)
+        adiacents.remove(node_to)
+
+    def get_properties(self, property, node, node_to=None):
+        if not node_to: #it's a vertex
+            return self.vertex_properties[node]
+        else:
+            return self.edge_properties[(node_from, node_to)]
+
+    def add_property(self, property, node, node_to=None):
+        if not node_to: #it's a vertex
+            self.vertex_properties[node].add(property)
+        else:
+            self.edge_properties[(node_from, node_to)].add(property)
+            
+    def remove_property(self, property, node, node_to=None):
+        if not node_to: #it's a vertex
+            self.vertex_properties[node].remove(property)
+        else:
+            self.edge_properties[(node_from, node_to)].remove(property)
+
+    def has_property(self, property, node, node_to=None):
+        if not node_to: #it's a vertex
+            if property in self.vertex_properties[node]:
+                return True
+            else:
+                return False
+        else:
+            if property in self.edge_properties[(node, node_to)]:
+                return True
+            else:
+                return False
+
+    def get_one_leaf(self):
+        for name in self.nodes:
+            if not self.edges[name]:
+                return name
+
+        return None
+
+
+
+
+
+class PacGraph(object):
+    def __init__(self, packages):
+        '''
+        packages is a list.
+        they'll be the only node in the graph with incoming degree = 0
+        '''
+        self.base_packages = packages
+        
+        self.graph = DiGraph() #an edge from a to b means "a depends on b"
+        
+        self._build()
+    
+    def get_dependencies(self, node):
+        '''return adiacent nodes on the graph'''
+        raise NotImplementedError
+        
+    def pop_leaf(self):
+        '''
+        find a "leaf" and return a tuple (leaf,)
+        If there isn't any leaf, return (the, smallest, cycle)
+        '''
+        graph = self.graph
+        for u in graph.nodes:
+            if graph.has_property('visited', u):
+                continue
+            for v in graph.get_adiacents(u):
+                if not graph.has_property('visited', v):
+                    break
+            else: #every adiacent has been visited, it's a leaf!
+                graph.add_property('visited', u)
+                return  u
+
+        return None
+
+
+    def _build(self):
+        '''build the graph, returns nothing'''
+        to_install = self._needed_packages(self.base_packages)#[('name','ver', 'repo'), ...]
+        for pkg in to_install:
+            for x in self._needed_packages((pkg,)):
+                if x != pkg:
+                    self.graph.add_edge(pkg, x)
+    
+    def _needed_packages(self, packages=None):
+        '''return a list of tuples (name,ver,repo) of the needed packages to install self.base_packages'''
+        needed = []
+        if packages:
+            pm = Popen('pacman -Sp %s' % (' '.join(packages)), stdout=PIPE, shell=True)
+        else:
+            pm = Popen('pacman -Sup %s', stdout=PIPE, shell=True)
+        for url in pm.stdout:
+            try:
+                url = url.strip().decode()
+            except UnicodeDecodeError:
+                #it is probably an informative string, not a URL. discard it
+                continue
+            mat = PKG_URL.search(url)
+            if mat:
+                name = mat.group(2)
+                needed.append(name)
+        return needed
+                    
+        
+class PacAtATime(object):
+    def __init__(self, packages):
+        '''
+        packages is the list of the packages to be installed.
+        Empty if it's a system upgrade
+        '''
+        self.installing = packages
+        self.graph = PacGraph(self.installing)
+    
+    def get_sequence(self):
+        l = []
+        while True:
+            leaf = self.graph.pop_leaf()
+            if not leaf:
+                break
+            l.append(leaf)
+        return l
+
+    def install(self):
+        '''install what is asked'''
+        to_install = self.get_sequence()
+        print to_install
+    
+    def n_packages(self):
+        '''return the number of packages to be installed '''
+        raise NotImplementedError
+    
+    def max_packages(self):
+        '''return the maximum number of packages to be installed in a single step '''
+        raise NotImplementedError
+    
+    def size(self):
+        '''return the total size of the packages to be installed'''
+        raise NotImplementedError
+        
+    def max_size(self):
+        '''return the maximum size to be installed in a single step'''
+        raise NotImplementedError
+
+    def _install_package(self, package_name):
+        raise NotImplementedError
+    
 def get_uris(args):
     if not args:
         cmd = "pacman -Sup"
@@ -126,9 +304,14 @@ def main():
     else:
         uris = get_uris([])
     packages = parse_input(uris)
-    
+
+    installer = PacAtATime(args)
+    if not options.pretend:
+        installer.install()
+
+    return 0
+    #OLD CODE (useful for PacAtATime?)
     os.system('mkdir -p %s' % BASE_DIR)
-    clean = 0
     if options.pretend:
         print "We're installing:"
         print ' '.join(packages.keys())
